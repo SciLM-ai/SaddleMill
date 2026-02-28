ecaa924aae3ade0cacef4cbe45dc3302c1498e96
Ilgar Baghishov <baghishov@utexas.edu>
2026-02-28 15:46:33 -0600

    Fixed run.sh for dimeropt_lematbul example

--- examples/dimeropt_lematbulk/run_vista.sh
+++ examples/dimeropt_lematbulk/run_vista.sh
@@ -8,4 +8,13 @@
 
 export LD_LIBRARY_PATH=/opt/apps/cuda/12.4/targets/sbsa-linux/lib/:$LD_LIBRARY_PATH
 
-srun -N $SLURM_NNODES -n $SLURM_NNODES --mpi=pmi2 flux start python -u -m tsearch
+export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps-pipe-$USER
+export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-mps-log-$USER
+srun -N $SLURM_NNODES -n $SLURM_NNODES mkdir -p $CUDA_MPS_PIPE_DIRECTORY
+srun -N $SLURM_NNODES -n $SLURM_NNODES mkdir -p $CUDA_MPS_LOG_DIRECTORY
+
+srun -N $SLURM_NNODES -n $SLURM_NNODES nvidia-cuda-mps-control -d
+
+srun -N $SLURM_NNODES -n $SLURM_NNODES flux start python -u -m tsearch
+
+srun -N $SLURM_NNODES -n $SLURM_NNODES bash -c "echo quit | nvidia-cuda-mps-control"
